670fc77bac9a049893c31334f9ad6cf3f9f58225
rky956678-afk <rky956678@gmail.com>
2026-05-25 15:48:48 +0530

    Remove unused imports from app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { type Express, type Request, type Response } from "express";
+import express, { type Express } from "express";
 import cors from "cors";
 const pinoHttp = require("pino-http");
 
